7a5d1b4005c44b98508a6d7eae3a51ba914fa025
playitsafe <playitsafe@qq.com>
2026-01-04 01:25:04 +0800

    fix time zone

--- src/logger.js
+++ src/logger.js
@@ -1,10 +1,27 @@
 const winston = require('winston');
 
+function formatTime(date = new Date()) {
+  const parts = new Intl.DateTimeFormat('en-CA', {
+    timeZone: 'Asia/Shanghai',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+  }).formatToParts(date);
+
+  const get = (type) => parts.find(p => p.type === type).value;
+
+  return `[${get('year')}-${get('month')}-${get('day')} ` + `${get('hour')}:${get('minute')}:${get('second')}]`;
+}
+
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
+      format: () =>formatTime(),
     }),
     winston.format.printf(
       info => `${info.timestamp} [${info.level.toUpperCase()}] ${info.message}`
